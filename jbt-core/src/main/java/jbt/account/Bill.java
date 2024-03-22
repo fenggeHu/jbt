8c1ddd8bb894928d4faf7aac6072b29ff7418525
max.hu <pbfoxhu@gmail.com>
2024-03-22 08:23:47 +0800

    加注释

--- jbt-core/src/main/java/jbt/account/Bill.java
+++ jbt-core/src/main/java/jbt/account/Bill.java
@@ -7,6 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
+ * 账单
+ *
  * @author jinfeng.hu  @date 2022/10/10
  **/
 @Data
@@ -15,10 +17,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Bill {
     String symbol;
+    // 交易日
     String datetime;
+    // 操作
     Action action;
+    // 买/卖的价格
     double price;
+    // 买1/卖-1的数量
     int quantity;
+    // 交易费用
     double fee;
+    // 总金额
     double total;
 }
