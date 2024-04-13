1a2128f6cc1083609a6f2d06ae82aa02e7400e02
hujinfeng <pbfoxhu@gmail.com>
2024-04-13 14:55:09 +0800

    检测代理类

--- jbt-core/src/main/java/jbt/notify/ProxyUtil.java
+++ jbt-core/src/main/java/jbt/notify/ProxyUtil.java
@@ -24,4 +24,11 @@ public class ProxyUtil {
 
         return proxyObj;
     }
+
+    // 判断一个对象是否是代理类的实例 TODO 不知道有没有更好的判断方法
+    public static boolean isProxyInstance(Object obj) {
+        // 检查对象的类是否是由 javassist 生成的代理类
+        String className = obj.getClass().getSimpleName();
+        return className.startsWith("com.sun.proxy.") || className.contains("_$$_");
+    }
 }
